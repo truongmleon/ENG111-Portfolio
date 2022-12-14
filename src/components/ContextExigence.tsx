65980b0de6a592c3688437ed08a0fc015e9630fd
Leon Truong <73002754+truongmleon@users.noreply.github.com>
2022-12-14 10:50:34 -0800

    moving paragraphs

--- src/components/ContextExigence.tsx
+++ src/components/ContextExigence.tsx
@@ -6,7 +6,7 @@ const ContextExigence = () => {
             </div>
             <article>
                 <div className="flex">
-                    <p className='context'>
+                    <p data-aos="fade-right" id='context'>
                     He graduated from Boston University in 2007 and started a blog. 
                     His posts were about dating-advice and in 2010 he transitioned to self-help writing. 
                     The internet was blowing up. 
@@ -14,12 +14,6 @@ const ContextExigence = () => {
                     Developing life advice with humor and such unique word choice, he started gaining a following as a million people started reading his blogs every month. 
                     It’s thanks to the internet that Manson was able provide self-help advice from his experiences, which lead to the growth of his audience. 
                     Manson basically went from being a broke college student to making crappy blogs to then becoming a self-help influencer.
-                    <br />
-                    <br />
-                    Manson viewed the self-help industry poorly as not very helpful to society. 
-                    The culture of mindless positivity just wasn’t very practical. 
-                    His book is a response to that – to help people choose what is important in life. 
-                    He wants to give a healthier perspective on pain, trauma, and problems. People struggle – he wants people to struggle better.
                     </p>
                 </div>
             </article>
