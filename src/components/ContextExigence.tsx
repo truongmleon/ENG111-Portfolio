26ee77785fcf0285479d1cb25002e78128ef0649
Leon Truong <73002754+truongmleon@users.noreply.github.com>
2022-12-14 07:44:39 -0800

    adding more to contexxt

--- src/components/ContextExigence.tsx
+++ src/components/ContextExigence.tsx
@@ -1,7 +1,28 @@
 const ContextExigence = () => {
     return (
         <div>
-            <h2>Why did Manson write this book?</h2>
+            <div className="flex">
+            <h2 data-aos="fade-down" id="context-header">Why did Manson write this book?</h2>
+            </div>
+            <article>
+                <div className="flex">
+                    <p className='context'>
+                    He graduated from Boston University in 2007 and started a blog. 
+                    His posts were about dating-advice and in 2010 he transitioned to self-help writing. 
+                    The internet was blowing up. 
+                    A place where one could find endless amounts of information was something people didn’t think was possible. 
+                    Developing life advice with humor and such unique word choice, he started gaining a following as a million people started reading his blogs every month. 
+                    It’s thanks to the internet that Manson was able provide self-help advice from his experiences, which lead to the growth of his audience. 
+                    Manson basically went from being a broke college student to making crappy blogs to then becoming a self-help influencer.
+                    <br />
+                    <br />
+                    Manson viewed the self-help industry poorly as not very helpful to society. 
+                    The culture of mindless positivity just wasn’t very practical. 
+                    His book is a response to that – to help people choose what is important in life. 
+                    He wants to give a healthier perspective on pain, trauma, and problems. People struggle – he wants people to struggle better.
+                    </p>
+                </div>
+            </article>
         </div>
     )
 }
