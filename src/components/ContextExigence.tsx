c546c45220eb96a978af751a4cf3b7b27b5d60fc
Leon <junzai12@icloud.com>
2022-12-14 19:14:33 -0800

    context conetent

--- src/components/ContextExigence.tsx
+++ src/components/ContextExigence.tsx
@@ -27,6 +27,21 @@ const ContextExigence = () => {
                     It’s thanks to the internet that Manson was able provide self-help advice from his experiences, which lead to the growth of his audience. 
                     Manson basically went from being a broke college student to making crappy blogs to then becoming a self-help influencer.
                     In the book <i>The Subtle Art of Not Giving a F*ck</i>, he tells numerous stories across many timelines to try to get the audience to understand what is being portrayed.
+                    The stories told are meaningful and help the audience relate with a sense of humor.
+                    <br />
+                    <br />
+                    An example of this is when Manson sets up the stage by talking about Charles Bukowski, an alcoholic, a chronic gambler, a lout, and a poet. 
+                    Bukowski wanted to be a writer, but his work was rejected by every journal, newspaper, publisher, and magazine. 
+                    It was said his work was terrible. Horrid. Boorish. Disgusting.
+                    Drinking alcohol was his way of coping after rejection after rejection.
+                    An editor showed curiosity to Bukowski and gave him a chance to publish his first novel, <i>Post Office</i>. 
+                    He went on and then published hundreds of poems and six novels. 
+                    Bukowski actually didn't care about his success.
+                    He was still having drink after drink.
+                    He was still sleeping with woman after woman.
+                    The act of not trying to be anyone else overcame the odds.
+                    Bukowski knew he was a loser and wholeheartedly accepted it as he didn’t try to change; he didn’t care. 
+                    Manson says Bukowski would be the last person to read a self-improvement book.
                     </p>
                     <div id="context-exigence"></div>
                 </div>
