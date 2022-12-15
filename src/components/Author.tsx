d48fc37a36e856620e75b04369b552100f095b6b
Leon <junzai12@icloud.com>
2022-12-14 16:12:22 -0800

    for some reason src folder has everything now

--- src/components/Author.tsx
+++ src/components/Author.tsx
@@ -2,14 +2,13 @@ const Author = () => {
     return (
         <div>
             <div className="flex">
-                <div id="author" ></div>
             <h2 data-aos="flip-right" id="author-header">Who is Mark Manson?</h2>
             </div>
             <div id="author" ></div>
             <img data-aos="flip-left" id='mark1' src="src/assets/manson1.png" alt="Mark Manson" />
             <article>
                 <div className="flex">
-                <p data-aos="flip-right" id="about-mark">Mark Manson is an American writer and blogger. Bloggling life advice was all he really did, but
+                <p data-aos="flip-right" id="about-mark">Mark Manson is an American self-help-self-proclaimed-psychologist-humor-writer and blogger. Bloggling life advice was all he really did out of college, but
                     in a few years, he started writing books as well.
                     His best selling book, <i>The Subtle Art of Not Giving a F*ck</i> sold more than 10 million copies globally.
                 </p>
--- src/components/ContextExigence.tsx
+++ src/components/ContextExigence.tsx
@@ -9,7 +9,7 @@ const ContextExigence = () => {
                     <p data-aos="fade-right" id='context'>
                     He graduated from Boston University in 2007 and started a blog. 
                     His posts were about dating-advice and in 2010 he transitioned to self-help writing. 
-                    The internet was blowing up. 
+                    The internet really started to gain traction around this time among younger generations. 
                     A place where one could find endless amounts of information was something people didn’t think was possible. 
                     Developing life advice with humor and such unique word choice, he started gaining a following as a million people started reading his blogs every month. 
                     It’s thanks to the internet that Manson was able provide self-help advice from his experiences, which lead to the growth of his audience. 
