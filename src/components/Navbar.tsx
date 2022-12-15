9ce90ace293bc63e23c78140ccad8e64c4afb276
Leon Truong <73002754+truongmleon@users.noreply.github.com>
2022-12-15 07:22:48 -0800

    purpose setup

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -27,5 +27,4 @@ const Header = () => {
   )
 }
 
-
 export default Header;
--- src/components/Purpose.tsx
+++ src/components/Purpose.tsx
@@ -1,7 +1,7 @@
 const Purpose = () => {
     return (
         <div>
-
+            <h2 id="purpose-header">Why?</h2>
         </div>
     )
 
