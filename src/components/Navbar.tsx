32ef8f5ef4ef350404ea5c94b1739a0b7434ead0
Leon Truong <73002754+truongmleon@users.noreply.github.com>
2022-12-16 13:59:02 -0800

    removed buy section

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,9 +3,6 @@ const Header = () => {
     <nav className="">
       <div id='nav'>
         <a className="nav-btn" href="#outline">Outline</a>
-          {
-          // Message, main idea
-          }
         <a className="nav-btn" href="#author">About the Author</a>
         <a className="nav-btn" href="#context-exigence-purpose">Context</a>
         <a className="nav-btn" href="#message">Message</a>
@@ -16,7 +13,6 @@ const Header = () => {
             three), and tone included. 
           */
         }
-        <a className="nav-btn" href="#buy">Buy</a>
         <a className="nav-btn" href="#references">References</a>
       </div>
     </nav>
