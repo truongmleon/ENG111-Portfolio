658c341dc8651ba2261db808e1d29faa5385d84f
Leon Truong <73002754+truongmleon@users.noreply.github.com>
2022-12-16 12:45:10 -0800

    some text

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -13,7 +13,17 @@ const Message = () => {
                 means being comfortable with being different
                 </h3>
                 <p className="subtlety-text">
-
+                Indifferent people want to be indifferent because they love to care. They care about what other people think about their ideas and style. In truth, there is nothing called not caring; it’s psychologically impossible to not care. Instead of not caring, we think about what we should care about. When people don’t care, they don’t care about the adversity for their goals. Everything not significant to them is dropped.
+                <br />
+                <br />
+                <i>"My mother was recently screwed out of a large chunk of money by a close  friend  of  hers.  Had  I  been  indifferent, I  would have  shrugged my shoulders, 
+                    sipped my mocha, and downloaded another season of The Wire. Sorry, Mom. But instead, I was indignant. I was pissed off. I said, “No, screw that, Mom. We’re going to lawyer the f*ck up and go after this a*shole."</i>
+                </p>
+                <h3 className="subtlety-header">
+                Subtlety #2: To not give a f*ck about adversity, you must first give a f*ck about something more important than adversity
+                </h3>
+                <p className="subtlety-text">
+                
                 </p>
             </article>
         </div>
