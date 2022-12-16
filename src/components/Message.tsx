0de9dbeffee7eae4c4456d6926acb2fd1c3ab516
Leon Truong <73002754+truongmleon@users.noreply.github.com>
2022-12-16 09:25:39 -0800

    thinking of message

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -8,7 +8,10 @@ const Message = () => {
             </div>
             <h3 data-aos="zoom-out-up" id="lead-in">Manson talked three subtleties that represented what not giving a f*ck meant</h3>
             <article>
-
+                <h3 className="subtlety-header">
+                Subtlety #1: Not giving a f*ck does not mean being indifferent; it
+                means being comfortable with being different
+                </h3>
             </article>
         </div>
     )
