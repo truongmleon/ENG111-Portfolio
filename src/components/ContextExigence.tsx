c9d48f1569b4838569e6c2558d2e27cee37bfa36
Leon Truong <73002754+truongmleon@users.noreply.github.com>
2022-12-16 07:58:59 -0800

    Update ContextExigence.tsx

--- src/components/ContextExigence.tsx
+++ src/components/ContextExigence.tsx
@@ -19,14 +19,24 @@ const ContextExigence = () => {
                 </div>
                 <div className="flex">
                     <p data-aos="fade-right" id='context'>
-                    Manson started a blog in 2007 to post about dating advice; fast-forward to 2010, he transitioned to self-help writing. The internet really started to gain traction around this time among younger generations, especially with the debut of the iPhone. Now, there was a place where one could efficiently find endless amounts of information in a matter of seconds. A quick search like 'How do I get my EX back?' would pop-up hundreds of results. As Manson was developing life advice with humor and such unique word choice on his blog, he started gaining a following as a million people read his blogs every month. It’s owed to the internet that Manson was able to provide self-help advice from his experiences, which led to the growth of his audience. Manson basically went from being a broke college student to making crappy blogs to becoming a self-help influencer. In the book The Subtle Art of Not Giving a F*ck, he tells numerous stories across various timelines to try to get the audience to understand what is being depicted. The stories told are meaningful and help the audience relate with a sense of humor.
+                    Manson started a blog in in 2007
+                    with posts about dating-advice and in 2010 he transitioned to self-help writing. 
+                    The internet really started to gain traction around this time among younger generations espically with the debut of the iPhone.
+                    Now, there was a place where one could efficiently find endless amounts of information in a matter of seconds.
+                    A quick search like "How do I get my EX back?" would pop up hundreds of resuts.
+                    As Manson was developing life advice with humor and such unique word choice, he started gaining a following as a million people started reading his blogs every month. 
+                    It’s thanks to the internet that Manson was able provide self-help advice from his experiences, which led to the growth of his audience. 
+                    Manson basically went from being a broke college student to making crappy blogs to then becoming a self-help influencer.
+                    In the book <i>The Subtle Art of Not Giving a F*ck</i>, he tells numerous stories across many timelines to try to get the audience to understand what is being portrayed.
+                    The stories told are meaningful and help the audience relate with a sense of humor.
                     <br />
                     <br />
                     Manson viewed the self-help industry poorly as not very helpful to society. The culture of mindless positivity just wasn’t very practical. His book is a response to that – to help people choose what is important in life. He wants to give a healthier perspective on pain, trauma, and problems. People struggle – he wants people to struggle better.
-                    The people who want self-help think something is fundamentally wrong with themselves or someone who is already good wants to elimate gaps to become a great person.
-                    
+                    The people who want self-help think something is wrong with themselves or someone who is already good wants to elimate gaps to become a better person.
+                    The person who thinks something is wrong with them lacks self acceptance
                     <br />
                     <br />
+
                     </p>
                     <div id="context-exigence-purpose"></div>
                 </div>
