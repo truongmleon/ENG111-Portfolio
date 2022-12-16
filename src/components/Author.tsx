6a4dded1b541660adb10230ce3176d3fd4fe1b56
Leon Truong <73002754+truongmleon@users.noreply.github.com>
2022-12-16 09:43:04 -0800

    text updates

--- src/components/Author.tsx
+++ src/components/Author.tsx
@@ -9,11 +9,11 @@ const Author = () => {
             <article>
                 <div className="flex">
                 <p data-aos="flip-right" id="about-mark">
-                Mark Manson is an American humor writer and blogger. After college, all he did was blog about life guidance, but after a while, he also began to write books. Manson writes for adults who desire self-improvement. He wants to give self-help advice while being humorous because he thinks advice is best presented in a fun manner. In his books, he depicts situations using profanity to make the text engaging with a serious outlook.
+                Mark Manson is an American humor writer and blogger. After college, all he did was blog about life guidance, but after a while, he also began to write books. Manson writes for adults who desire self-improvement or people who just have too much stuff on their plate. He wants to give self-help advice while being humorous because he thinks advice is best presented in a fun manner. In his books, he depicts situations using profanity to make the text engaging with a serious outlook.
                 </p>
                 </div>
                 <div className="flex">
-                <p data-aos="flip-left" id="quote"><i>"Everyone and their TV commercial wants you to believe that the key to a
+                <p data-aos="flip-left" className="quote"><i>"Everyone and their TV commercial wants you to believe that the key to a
 good life is a nicer job, or a more rugged car, or a prettier girlfriend, or a hot
 tub with an inflatable pool for the kids. The world is constantly telling you
 that the path to a better life is more, more, more—buy more, own more, make
--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -3,7 +3,7 @@ const Message = () => {
         <div>
             <div id="message-titles">
             <div id="message"></div>
-            <h2 data-aos="zoom-in" id='message-header-one'>WHAT WAS THE</h2>
+            <h2 data-aos="zoom-in" id='message-header-one'>WHAT IS THE</h2>
             <h2 data-aos="zoom-in" id='message-header-two'>MESSAGE?</h2>
             </div>
             <h3 data-aos="zoom-out-up" id="lead-in">Manson talked three subtleties that represented what not giving a f*ck meant</h3>
@@ -12,6 +12,9 @@ const Message = () => {
                 Subtlety #1: Not giving a f*ck does not mean being indifferent; it
                 means being comfortable with being different
                 </h3>
+                <p className="subtlety-text">
+
+                </p>
             </article>
         </div>
     )
