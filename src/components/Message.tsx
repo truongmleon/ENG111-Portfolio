0a70dc813f09bfc01293ea039dbddece958f9967
Leon Truong <73002754+truongmleon@users.noreply.github.com>
2022-12-16 13:34:07 -0800

    Update Message.tsx

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -8,30 +8,30 @@ const Message = () => {
             </div>
             <h3 data-aos="zoom-out-up" id="lead-in">Manson talked three subtleties that represented what not giving a f*ck meant</h3>
             <article>
-                <h3 className="subtlety-header">
+                <h3 data-aos="zoom-out-up" className="subtlety-header">
                 Subtlety #1: Not giving a f*ck does not mean being indifferent; it
                 means being comfortable with being different
                 </h3>
-                <p className="subtlety-text">
+                <p data-aos="zoom-out-up" className="subtlety-text">
                 Indifferent people want to be indifferent because they love to care. They care about what other people think about their ideas and style. In truth, there is nothing called not caring; it’s psychologically impossible to not care. Instead of not caring, we think about what we should care about. When people don’t care, they don’t care about the adversity for their goals. Everything not significant to them is dropped.
                 <br />
                 <br />
                 <i>"My mother was recently screwed out of a large chunk of money by a close  friend  of  hers.  Had  I  been  indifferent, I  would have  shrugged my shoulders, 
                     sipped my mocha, and downloaded another season of The Wire. Sorry, Mom. But instead, I was indignant. I was pissed off. I said, “No, screw that, Mom. We’re going to lawyer the f*ck up and go after this a*shole."</i>
                 </p>
-                <h3 className="subtlety-header">
+                <h3 data-aos="zoom-out-up" className="subtlety-header">
                 Subtlety #2: To not give a f*ck about adversity, you must first give a f*ck about something more important than adversity
                 </h3>
-                <p className="subtlety-text">
+                <p data-aos="zoom-out-up" className="subtlety-text">
                 People who find themselves constantly handing out the “I care” card do not have much they care for deeply. Many of our “life problems” take our limited attention, but they don’t matter that much. Figuring out something substantial to care about is a more productive use of time compared to caring about small meaningless causes that contribute little to nothing to life.
                 <br />
                 <br />
                 <i>"Imagine you’re at  a  grocery store,  and  you  watch an  elderly  lady scream at the cashier, berating him for not accepting her thirty-cent coupon. Why does this lady give a f*ck? It’s just thirty cents. I’ll tell you why: That lady probably doesn’t have anything better to do with her days than to sit at home cutting out coupons."</i>
                 </p>
-                <h3 className="subtlety-header">
+                <h3 data-aos="zoom-out-up" className="subtlety-header">
                 Subtlety #3: Whether you realize it or not, you are always choosing what to give a f*ck about
                 </h3>
-                <p className="subtlety-text">
+                <p data-aos="zoom-out-up" className="subtlety-text">
                 When we were kids, every little thing appeared to matter SO much because they were new and exciting. So, we cared about everything and everyone. When we grow older, we realize much of the stuff we cared so much as a kid merely has a small lasting impact. This means we grow and start getting selective about what we care about, readjusting what is significant to us. Maturity.
                 <br />
                 <br />
