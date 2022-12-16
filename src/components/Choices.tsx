6391bcaa7d68cdcf326661b13be604407a6db01b
Leon Truong <73002754+truongmleon@users.noreply.github.com>
2022-12-16 13:46:00 -0800

    small chnage

--- src/components/Choices.tsx
+++ src/components/Choices.tsx
@@ -1,6 +1,8 @@
 const Choices = () => {
     return (
-        <div></div>
+        <div>
+
+        </div>
     )
 }
 
--- src/components/ContextExigence.tsx
+++ src/components/ContextExigence.tsx
@@ -19,7 +19,7 @@ const ContextExigence = () => {
                 </div>
                 <div className="flex">
                     <p data-aos="fade-right" id='context'>
-                    Manson started a blog in 2007 to post about dating advice; fast-forward to 2010, he transitioned to self-help writing. The internet really started to gain traction around this time among younger generations, especially with the debut of the iPhone. Now, there was a place where one could efficiently find endless amounts of information in a matter of seconds. A quick search like 'How do I get my EX back?' would pop-up hundreds of results. As Manson was developing life advice with humor and such unique word choice on his blog, he started gaining a following as a million people read his blogs every month. It’s owed to the internet that Manson was able to provide self-help advice from his experiences, which led to the growth of his audience. Manson basically went from being a broke college student to making crappy blogs to becoming a self-help influencer. In the book The Subtle Art of Not Giving a F*ck, he tells numerous stories across various timelines to try to get the audience to understand what is being depicted. The stories told are meaningful and help the audience relate with a sense of humor.
+                    Manson started a blog in 2007 to post about dating advice; fast-forward to 2010, he transitioned to self-help writing. The internet really started to gain traction around this time among younger generations, especially with the debut of the iPhone. Now, there was a place where one could efficiently find endless amounts of information in a matter of seconds. A quick search like "How do I get my EX back?"" would pop-up hundreds of results. As Manson was developing life advice with humor and such unique word choice on his blog, he started gaining a following as a million people read his blogs every month. It’s owed to the internet that Manson was able to provide self-help advice from his experiences, which led to the growth of his audience. Manson basically went from being a broke college student to making crappy blogs to becoming a self-help influencer. <i>In the book The Subtle Art of Not Giving a F*ck</i>, he tells numerous stories across various timelines to try to get the audience to understand what is being depicted. The stories told are meaningful and help the audience relate with a sense of humor.
                     The connections made between the audience and the book help people choose what is important on their plate.
                     <br />
                     <br />
@@ -31,28 +31,8 @@ const ContextExigence = () => {
                 <div className="flex">
                 <p className="quote">
                     <i>
-                "This book will help you think a little bit more clearly 
-about what you’re
-choosing 
-to  find  important 
-in  life  and  what  you’re 
-choosing  to  find
-unimportant.
-I  believe 
-that  today  we’re  facing 
-a  psychological 
-epidemic, 
-one  in
-which people no longer 
-realize 
-it’s okay for things to suck sometimes. 
-I
-know that sounds 
-intellectually 
-lazy on the surface, 
-but I promise 
-you, it’s a
-life/death sort of issue."</i>
+                "This book will help you think a little bit more clearly about what you’re choosing to  find  important in  life  and  what  you’re choosing  to  find unimportant. I  believe that  today  we’re  facing a  psychological epidemic, one  in which people no longer 
+                realize it’s okay for things to suck sometimes. I know that sounds intellectually lazy on the surface, but I promise you, it’s alife/death sort of issue."</i>
                 </p>
                 </div>
             </article>
