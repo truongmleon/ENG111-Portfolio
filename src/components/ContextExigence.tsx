f8f36fdd73dd4b59b0daf8d6cfd3bdb8b3a516a0
Leon <junzai12@icloud.com>
2022-12-15 22:25:44 -0800

    more text

--- src/components/ContextExigence.tsx
+++ src/components/ContextExigence.tsx
@@ -19,16 +19,7 @@ const ContextExigence = () => {
                 </div>
                 <div className="flex">
                     <p data-aos="fade-right" id='context'>
-                    Manson started a blog in in 2007
-                    with posts about dating-advice and in 2010 he transitioned to self-help writing. 
-                    The internet really started to gain traction around this time among younger generations espically with the debut of the iPhone.
-                    Now, there was a place where one could efficiently find endless amounts of information in a matter of seconds.
-                    A quick search like "How do I get my EX back?" would pop up hundreds of resuts.
-                    As Manson was developing life advice with humor and such unique word choice, he started gaining a following as a million people started reading his blogs every month. 
-                    It’s thanks to the internet that Manson was able provide self-help advice from his experiences, which led to the growth of his audience. 
-                    Manson basically went from being a broke college student to making crappy blogs to then becoming a self-help influencer.
-                    In the book <i>The Subtle Art of Not Giving a F*ck</i>, he tells numerous stories across many timelines to try to get the audience to understand what is being portrayed.
-                    The stories told are meaningful and help the audience relate with a sense of humor.
+                    Manson started a blog in 2007 to post about dating advice; fast-forward to 2010, he transitioned to self-help writing. The internet really started to gain traction around this time among younger generations, especially with the debut of the iPhone. Now, there was a place where one could efficiently find endless amounts of information in a matter of seconds. A quick search like 'How do I get my EX back?' would pop-up hundreds of results. As Manson was developing life advice with humor and such unique word choice on his blog, he started gaining a following as a million people read his blogs every month. It’s owed to the internet that Manson was able to provide self-help advice from his experiences, which led to the growth of his audience. Manson basically went from being a broke college student to making crappy blogs to becoming a self-help influencer. In the book The Subtle Art of Not Giving a F*ck, he tells numerous stories across various timelines to try to get the audience to understand what is being depicted. The stories told are meaningful and help the audience relate with a sense of humor.
                     <br />
                     <br />
                     Manson viewed the self-help industry poorly as not very helpful to society. The culture of mindless positivity just wasn’t very practical. His book is a response to that – to help people choose what is important in life. He wants to give a healthier perspective on pain, trauma, and problems. People struggle – he wants people to struggle better.
@@ -36,18 +27,6 @@ const ContextExigence = () => {
                     
                     <br />
                     <br />
-                    An example of this is when Manson sets up the stage by talking about Charles Bukowski, an alcoholic, a chronic gambler, a lout, and a poet. 
-                    Bukowski wanted to be a writer, but his work was rejected by every journal, newspaper, publisher, and magazine. 
-                    It was said his work was terrible. Horrid. Boorish. Disgusting.
-                    Drinking alcohol was his way of coping after rejection after rejection.
-                    An editor showed curiosity to Bukowski and gave him a chance to publish his first novel, <i>Post Office</i>. 
-                    He went on and then published hundreds of poems and six novels. 
-                    Bukowski actually didn't care about his success.
-                    He was still having drink after drink.
-                    He was still sleeping with woman after woman.
-                    The act of not trying to be anyone else overcame the odds.
-                    Bukowski knew he was a loser and wholeheartedly accepted it as he didn’t try to change; he didn’t care. 
-                    Manson says Bukowski would be the last person to read a self-improvement book.
                     </p>
                     <div id="context-exigence-purpose"></div>
                 </div>
