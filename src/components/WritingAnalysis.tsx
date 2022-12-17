732b774d1cbde29632a1b7c95926a9fc29ff97a4
Leon <junzai12@icloud.com>
2022-12-16 22:00:55 -0800

    appeals

--- src/components/WritingAnalysis.tsx
+++ src/components/WritingAnalysis.tsx
@@ -60,9 +60,21 @@ const Writing = () => {
                 </div>
             </div>
             </article>
-            
             <article>
             <h3 data-aos="zoom-in" className="analysis-header">Appeals</h3>
+                <div className="flex">
+                    <div data-aos="zoom-in" id="content-appeals"> 
+                        <p data-aos="fade-left" className="content-text">
+                        Manson often helps the audience self-reflect with some reasoning. This allows the audience to develop an intuition the next time they face a similar problem. It ties in with how throughout the book Manson describes situations with aphorisms.
+                        An example of this is when Manson was discussing improvement requiring numerous failures to succeed. The next time the audience thinks “they are better than me,” they reflect and think it’s because they put in the effort, so they should too. 
+                        <br />
+                        <br />
+                        <i>
+                            "If someone is better than you at something, then it’s likely because she has failed at it more than you have. If someone is worse than you, it’s likely because he hasn’t been through all of the painful learning experiences you have."
+                        </i>
+                        </p>    
+                    </div>
+                </div>
             </article>
         </div>
     )
