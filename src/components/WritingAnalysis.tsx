324b4b58743fc8b4a7fab88bf04ee85547ed7e89
Leon <junzai12@icloud.com>
2022-12-16 19:11:23 -0800

    content box

--- src/components/WritingAnalysis.tsx
+++ src/components/WritingAnalysis.tsx
@@ -3,27 +3,39 @@ const Writing = () => {
         <div>
             <div id="writing"></div>
             <br />
+            <br />
             <h2 data-aos="zoom-out-up"  id="writing-header">WRITING ANALYSIS</h2>
             <article>
-            <h3 className="analysis-header">Rhetorical Chooooooooices</h3>
+            <h3 data-aos="zoom-in" className="analysis-header">Rhetorical Chooooooooices</h3>
             {
                 //Arrangements, Devices, Diction
             }
             <div className="flex">
-                <div id="content-arrangements">
-                <h4 className="content-titles">Arrangement: exemplification</h4>
+                <div data-aos="zoom-in" id="content-arrangements">
+                <h4 data-aos="fade-right" className="content-titles">Arrangement: exemplification</h4>
+                <p data-aos="fade-left" className="content-text">
+                    pftcxiujhvjiuoiuiohihuiigvuk8y9go8u87goy
+                </p>
                 </div>
             </div>
             <br />
+            <br />
             <div className="flex">
-                <div id="content-devices">
-                <h4 className="content-titles">Devices: </h4>
+                <div data-aos="zoom-in" id="content-devices">
+                <h4 data-aos="fade-right" className="content-titles">Devices: </h4>
+                <p data-aos="fade-left" className="content-text">
+                    pftcxiujhvjiuoiuiohihuiigvuk8y9go8u87goy
+                </p>
                 </div>
             </div>
             <br />
+            <br />
             <div className="flex">
-                <div id="content-diction">
-                <h4 className="content-titles">Diction: fun word choice</h4>
+                <div data-aos="zoom-in" id="content-diction">
+                <h4 data-aos="fade-right" className="content-titles">Diction: fun word choice</h4>
+                <p data-aos="fade-left" className="content-text">
+                    pftcxiujhvjiuoiuiohihuiigvuk8y9go8u87goy
+                </p>
                 </div>
             </div>
             </article>
