a9cebf8fe51c8c857e011a83546f686b738c9c29
Leon <junzai12@icloud.com>
2022-12-16 18:45:58 -0800

    choices

--- src/components/WritingAnalysis.tsx
+++ src/components/WritingAnalysis.tsx
@@ -4,6 +4,23 @@ const Writing = () => {
             <div id="writing"></div>
             <br />
             <h2 data-aos="zoom-out-up"  id="writing-header">WRITING ANALYSIS</h2>
+            <article>
+            <h3 className="analysis-header">Rhetorical Chooooooooices</h3>
+            {
+                //Arrangements, Devices, Diction
+            }
+            <div id="context-arrangements">
+            
+            </div>
+
+            <div id="context-devices">
+
+            </div>
+
+            <div id="context-diction">
+
+            </div>
+            </article>
         </div>
     )
 }
