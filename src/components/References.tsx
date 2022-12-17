ac42daffedd7e17c2322e0574f42f42f2abb5c12
Leon <junzai12@icloud.com>
2022-12-16 23:44:45 -0800

    Update References.tsx

--- src/components/References.tsx
+++ src/components/References.tsx
@@ -4,8 +4,8 @@ const References = () => {
             <h3 data-aos="zoom-in" id="ref-header">References</h3>
             <ul data-aos="zoom-in">
                 <li><a target="_blank" className="link" href="https://github.com/truongmleon/APLAC-Book-Project">Coded with &lt;3 by Leon Truong using React.js </a></li>
-                <li><a target="_blank" className="link" href="https://markmanson.net/books/subtle-art">The Subtle Art of Not Giving a F*ck</a></li>
-                <li><a target="_blank" className="link" href="https://youtu.be/a3Z7zEc7AXQ">The Subtle Art of Not Giving a F*ck (2nd link)</a></li>
+                <li><a target="_blank" className="link" href="https://markmanson.net/books/subtle-art"><i>The Subtle Art of Not Giving a F*ck</i></a></li>
+                <li><a target="_blank" className="link" href="https://youtu.be/a3Z7zEc7AXQ"><i>The Subtle Art of Not Giving a F*ck</i> (2nd link)</a></li>
                 <li><a target="_blank" className="link" href="https://markmanson.net/about">https://markmanson.net/about</a></li>
                 <li><a target="_blank" className="link" href="https://markmanson.net/self-help">https://markmanson.net/self-help</a></li>
                 <li><a target="_blank" className="link" href="https://baos.pub/5-powerful-lessons-i-got-from-the-subtle-art-of-not-giving-a-f-ck-502911dd05fc">https://baos.pub/5-powerful-lessons-i-got-from-the-subtle-art-of-not-giving-a-f-ck-502911dd05fc</a></li>
