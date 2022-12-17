70747ef2bb0c39a9c422726c7396879740f7b502
Leon <junzai12@icloud.com>
2022-12-16 20:41:53 -0800

    arangemeghfejklk

--- src/components/WritingAnalysis.tsx
+++ src/components/WritingAnalysis.tsx
@@ -12,9 +12,17 @@ const Writing = () => {
             }
             <div className="flex">
                 <div data-aos="zoom-in" id="content-arrangements">
-                <h4 data-aos="fade-right" className="content-titles">Arrangement: exemplification</h4>
+                <h4 data-aos="fade-right" className="content-titles">Arrangement: Exemplification</h4>
                 <p data-aos="fade-left" className="content-text">
-                    pftcxiujhvjiuoiuiohihuiigvuk8y9go8u87goy
+                Anecdotes. They help make the text more appealing and interesting to read. Manson proves numerous anecdotes to put the audience into perspective. The way the exemplification is provided feels sophisticated and insightful, so the audience can take away crucial information efficiently.
+                He sets the stage by talking about Charles Bukowski, an alcoholic, a chronic gambler, a lout, and a poet. Bukowski wanted to be a writer, but his work was rejected by every journal and newspaper. It was said his work was terrible. Horrid. Boorish. Disgusting. Drinking alcohol was his way of coping after rejection after rejection. An editor showed curiosity to Bukowski and gave him a chance to publish his first novel, <i>Post Office</i>. He went on and published hundreds of poems and six novels. Bukowski didn't care about his success. He was still drinking after drinking, gambling away. Bukowski knew he was a loser and wholeheartedly accepted it as he didn’t try to change because he didn't care.
+                <br />
+                <br />
+                <i>"He never tried to be anything other than what he was. The genius in
+                    Bukowski’s work was not in overcoming unbelievable odds or developing
+                    himself into a shining literary light. It was the opposite. It was his simple
+                    ability to be completely, unflinchingly honest with himself—especially the
+                    worst parts of himself—and to share his failings without hesitation or doubt."</i>
                 </p>
                 </div>
             </div>
@@ -22,7 +30,7 @@ const Writing = () => {
             <br />
             <div className="flex">
                 <div data-aos="zoom-in" id="content-devices">
-                <h4 data-aos="fade-right" className="content-titles">Devices: </h4>
+                <h4 data-aos="fade-right" className="content-titles">Devices: Aphorism</h4>
                 <p data-aos="fade-left" className="content-text">
                     pftcxiujhvjiuoiuiohihuiigvuk8y9go8u87goy
                 </p>
