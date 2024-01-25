d4782325ae63fc2c872b4921f713f5767d42bb5f
Leon <junzai12@icloud.com>
2024-01-24 22:33:32 -0800

    Update OutcomeOne.tsx

--- src/components/OutcomeOne.tsx
+++ src/components/OutcomeOne.tsx
@@ -1,5 +1,3 @@
-import mp from "/src/public/port/13thmeeting.pdf";
-
 const OutcomeOne = () => {
     return (
         <div>
@@ -41,7 +39,7 @@ const OutcomeOne = () => {
             <div className="flex">
                 <div className="files" id="files-one">
                     <h2>Outcome One Files:</h2>
-                    <a href="">Major Paper One</a>
+                    <a href="/src/files/UWMajorPaper.docx" target="_blank"><u>Major Paper One</u></a>
                     <br />
                     <a href="">Major Paper One Revision</a>
                 </div>
