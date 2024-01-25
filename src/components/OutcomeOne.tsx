82d2aeb6af0e872f33df90830cc5151280f957aa
Leon <junzai12@icloud.com>
2024-01-24 22:54:24 -0800

    another test

--- src/components/OutcomeOne.tsx
+++ src/components/OutcomeOne.tsx
@@ -1,4 +1,4 @@
-import mp from "/src/files/UWMajorPaper.docx";
+import { majorPaper } from "../main";
 
 const OutcomeOne = () => {
     return (
@@ -41,7 +41,7 @@ const OutcomeOne = () => {
             <div className="flex">
                 <div className="files" id="files-one">
                     <h2>Outcome One Files:</h2>
-                    <a href={mp}><u>Major Paper One</u></a>
+                    <a href={majorPaper}><u>Major Paper One</u></a>
                     <br />
                     <a href="">Major Paper One Revision</a>
                 </div>
--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,10 @@ import OutcomeThree from './components/OutcomeThree';
 import OutcomeFour from './components/OutcomeFour';
 import { useState } from 'react';
 
+const majorPaper = require("/src/files/UWMajorPaper.docx");
+
+export { majorPaper };
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Navbar />
